5c8e62d6dbe766bc468484dcfd3bccd69915b348
padmini <root@ip-172-31-43-136.ap-south-1.compute.internal>
2020-02-13 08:54:59 +0000

    initial commit

---
+++ optical.sh
@@ -0,0 +1,15 @@
+echo "storage capacity of device"
+
+a=750mb
+b=4.7gb
+if [$a gt $b]
+then
+
+echo "cd has high capacity"
+
+else
+
+echo "dvd has high capacity"
+fi
+
+
---
+++ route.sh
@@ -0,0 +1,12 @@
+echo "if else condition starts"
+
+a=10
+b=20
+if [$a -gt $b]
+then
+echo "$a is greaterthen $b"
+else
+echo "$a is lesserthen $b"
+fi 
+
+echo "if else condition over"
